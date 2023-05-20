725e8dc6c83199faf3f1d2ec2a0a0f0fbe09c76c
bearsnowstorm <dontemailme@throwaway.com>
2023-05-20 21:28:02 +1000

    After exercise 3 - also inelegant

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -42,17 +42,30 @@ const ShoeIndex = ({ sortId, setSortId }) => {
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: flex-end;
+`;
 
-const LeftColumn = styled.div``;
+const LeftColumn = styled.div`
+  padding-top: 30px;
+`;
 
-const MainColumn = styled.div``;
+const MainColumn = styled.div`
+  padding: 16px;
+`;
 
-const Header = styled.header``;
+const Header = styled.header`
+  display: flex;
+  gap: 8px;
+  align-items: baseline;
+`;
 
 const Title = styled.h2`
   font-size: 1.5rem;
   font-weight: ${WEIGHTS.medium};
+  margin-right: auto;
 `;
 
 export default ShoeIndex;
